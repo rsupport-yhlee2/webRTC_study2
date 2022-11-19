038faca2f87bcccb6ca4f4786448c988c9c5c385
yhlee2 <yhlee2@rsupport.com>
2022-11-19 17:53:45 +0900

    Connect 화면에서 퍼미션 요청 및 클라이언트 초기화

--- app/src/main/java/com/example/webrtcstudy2/ConnectActivity.kt
+++ app/src/main/java/com/example/webrtcstudy2/ConnectActivity.kt
@@ -1,14 +1,61 @@
 package com.example.webrtcstudy2
 
+import android.content.pm.PackageManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 import com.example.webrtcstudy2.databinding.ActivityConnectBinding
+import java.util.jar.Manifest
 
 class ConnectActivity : AppCompatActivity() {
     private lateinit var binding: ActivityConnectBinding
+
+    private lateinit var roomID: String
+    private var isJoin: Boolean = false
+
+    private lateinit var rtcClient: RtcClient
+    private lateinit var signalingClient: SignalingClient
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityConnectBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        getIntentData()
+        checkPermissions()
+    }
+
+    private fun getIntentData() {
+        roomID = intent.getStringExtra("roomID")!!
+        isJoin = intent.getBooleanExtra("isJoin", false)
+    }
+
+    private fun checkPermissions() {
+        if (ContextCompat.checkSelfPermission(
+                this,
+                android.Manifest.permission.CAMERA
+            ) != PackageManager.PERMISSION_GRANTED &&
+            ContextCompat.checkSelfPermission(
+                this,
+                android.Manifest.permission.RECORD_AUDIO
+            ) != PackageManager.PERMISSION_GRANTED
+        ) {
+            requestCameraAndAudioPermisson()
+        } else {
+            initializeClients()
+        }
+    }
+
+    private fun initializeClients() {
+        rtcClient = RtcClient()
+        signalingClient = SignalingClient(roomID, object : SignalListenr {})
+    }
+
+    private fun requestCameraAndAudioPermisson() {
+        ActivityCompat.requestPermissions(
+            this,
+            arrayOf(android.Manifest.permission.CAMERA, android.Manifest.permission.RECORD_AUDIO),
+            1
+        )
     }
 }
