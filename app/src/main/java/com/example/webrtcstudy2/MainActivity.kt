b6a8c04eae350aecbfbd12359d033e85f4362fa4
yhlee2 <yhlee2@rsupport.com>
2022-11-19 17:26:18 +0900

    UI setting

--- app/src/main/java/com/example/webrtcstudy2/MainActivity.kt
+++ app/src/main/java/com/example/webrtcstudy2/MainActivity.kt
@@ -3,12 +3,20 @@ package com.example.webrtcstudy2
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.webrtcstudy2.databinding.ActivityMainBinding
+import com.google.firebase.database.ktx.database
+import com.google.firebase.ktx.Firebase
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
+    private val database = Firebase.database.reference
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        initViews()
+    }
+
+    private fun initViews() = with(binding) {
+
     }
 }
